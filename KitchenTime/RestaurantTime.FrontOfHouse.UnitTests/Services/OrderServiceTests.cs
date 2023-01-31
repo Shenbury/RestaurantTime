b738fcbc92c7da12e12e3cb55744964072488605
Shenbury <simonhenbury@live.co.uk>
2023-01-31 14:42:32 +0000

    OrderServiceTests

--- KitchenTime/RestaurantTime.FrontOfHouse.UnitTests/Services/OrderServiceTests.cs
+++ KitchenTime/RestaurantTime.FrontOfHouse.UnitTests/Services/OrderServiceTests.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Logging.Abstractions;
 using RestaurantTime.Database.Services;
 using RestaurantTime.Database;
 using RestaurantTime.Shared.Dtos.OrderDto;
-using RestaurantTime.TestingLibraries.MockData;
 
 namespace RestaurantTime.FrontOfHouse.Services.UnitTests.Services
 {
@@ -18,25 +17,40 @@ namespace RestaurantTime.FrontOfHouse.Services.UnitTests.Services
             Task.Run(() => SetupCleanOrderService()).Wait();
         }
 
-        [Theory]
-        [InlineData()]
+        [Fact]
         public async Task CanCreateOrder_WithFreshOrderData()
         {
             // Arrange
-            var createOrderDto = new CreateOrderDto(1, 1, new List<int>(1), new List<int>(1), DateTime.Now, null);
+            var createOrderDto = new CreateOrderDto(1, 1, new List<int> { 1 }, new List<int> { 1 }, DateTime.Now, null);
 
             // Act
             var getOrderDto = await _orderService.CreateOrder(createOrderDto);
 
             // Assert
-            Assert.NotNull(getOrderDto);
+            Assert.Equal(createOrderDto.GuestId, getOrderDto.GuestId);
+            Assert.Equal(createOrderDto.WaiterId, getOrderDto.WaiterId);
+            Assert.Equal(createOrderDto.FoodIds.First(), getOrderDto.Food.First().Id);
+            Assert.Equal(createOrderDto.DrinkIds.First(), getOrderDto.Drinks.First().Id);
+            Assert.Equal(createOrderDto.HasOrderedFood, getOrderDto.HasOrderedFood);
+            Assert.Equal(createOrderDto.InKitchen, getOrderDto.InKitchen);
+            Assert.Equal(createOrderDto.PlatesTakenAway, getOrderDto.PlatesTakenAway);
+            Assert.Equal(createOrderDto.BeenServed, getOrderDto.BeenServed);
         }
 
         [Theory]
-        [InlineData()]
-        public void CantCreateOrder_With_()
+        [InlineData(null, 1, new[] { 1 }, new[] { 1 })]
+        [InlineData(1, null, new[] { 1 }, new[] { 1 })]
+        [InlineData(1, null, null, new[] { 1 })]
+        [InlineData(1, null, null, null)]
+        public async Task CantCreateOrder_WithNullRequiredParameters_(int guestId, int waiterId, int[] foodIds, int[] drinkIds)
         {
+            // Arrange & Act & Assert
 
+            await Assert.ThrowsAnyAsync<Exception>(async () =>
+            {
+                var createOrderDto = new CreateOrderDto(guestId, waiterId, foodIds.ToList(), drinkIds.ToList(), DateTime.Now, null);
+                var getOrderDto = await _orderService.CreateOrder(createOrderDto);
+            });
         }
 
         private async Task SetupCleanOrderService()
@@ -45,7 +59,7 @@ namespace RestaurantTime.FrontOfHouse.Services.UnitTests.Services
             new InMemoryRestaurantDbContext()
             .Initialize();
 
-            await new BaseDataToRestaurantDbContext().AddAdditionalBaseDataToDbContext(_restaurantDbContext);
+            //await new BaseDataToRestaurantDbContext().AddAdditionalBaseDataToDbContext(_restaurantDbContext);
 
             var repo = new OrderRepository(_restaurantDbContext, new NullLogger<OrderRepository>());
             _orderService = new OrderService(repo, new NullLogger<OrderService>());
