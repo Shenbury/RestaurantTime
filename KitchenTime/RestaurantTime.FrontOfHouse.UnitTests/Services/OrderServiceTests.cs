0047956081477b779326b7fcc713971499028119
Shenbury <simonhenbury@live.co.uk>
2023-01-31 13:29:46 +0000

    TestingLibraries

---
+++ KitchenTime/RestaurantTime.FrontOfHouse.UnitTests/Services/OrderServiceTests.cs
@@ -0,0 +1,51 @@
+using RestaurantTime.TestingLibraries.UnitTestFramework;
+using RestaurantTime.FrontOfHouse.Services.Services;
+using Microsoft.Extensions.Logging.Abstractions;
+using RestaurantTime.Database.Services;
+using RestaurantTime.Database;
+using RestaurantTime.Shared.Dtos.OrderDto;
+
+namespace RestaurantTime.FrontOfHouse.Services.UnitTests.Services
+{
+    public class OrderServiceTests
+    {
+        private RestaurantDbContext _restaurantDbContext;
+        private OrderService _orderService;
+
+        public OrderServiceTests()
+        {
+            SetupCleanOrderService();
+        }
+
+        [Theory]
+        [InlineData()]
+        public async Task CanCreateOrder_WithFreshOrder()
+        {
+            // Arrange
+            var createOrderDto = new CreateOrderDto();
+
+            // Act
+            var getOrderDto = await _orderService.CreateOrder(createOrderDto);
+
+            // Assert
+            Assert.NotNull(createOrderDto);
+        }
+
+        [Theory]
+        [InlineData()]
+        public void CantCreateOrder_With_()
+        {
+
+        }
+
+        private void SetupCleanOrderService()
+        {
+            _restaurantDbContext =
+            new InMemoryRestaurantDbContext()
+            .Initialize();
+
+            var repo = new OrderRepository(_restaurantDbContext, new NullLogger<OrderRepository>());
+            _orderService = new OrderService(repo, new NullLogger<OrderService>());
+        }
+    }
+}
---
+++ KitchenTime/RestaurantTime.TestingLibraries/UnitTestFramework/InMemoryRestaurantDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantTime.Database;
+
+namespace RestaurantTime.TestingLibraries.UnitTestFramework
+{
+    public class InMemoryRestaurantDbContext
+    {
+        public RestaurantDbContext Initialize() 
+        {
+            DbContextOptions<RestaurantDbContext> options = new DbContextOptionsBuilder<RestaurantDbContext>()
+            .UseInMemoryDatabase(databaseName: "RestaurantTime")
+            .Options;
+
+            var context = new RestaurantDbContext(options);
+
+            return context;
+        }
+    }
+}
